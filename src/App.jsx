1b8581a515658c61d416db25c90e0692dbbf67a6
Vishal Kalita <vishalkalita036@gmail.com>
2024-09-22 16:31:34 +0530

    added routes for about,events

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import Home from "./components/Home";
+import About from "./components/About";
+import Events from "./components/Events";
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -8,29 +12,34 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 5000);
+    }, 4000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <>
+    <BrowserRouter>
       {isLoading ? (
-        <div className="gradient gradient-background"
-        style={{ "--degree": `${210}deg` }}>
-          <Loader/>
-          <Home/>
+        <div
+          className="gradient gradient-background"
+          style={{ "--degree": `${210}deg` }}
+        >
+          <Loader />
         </div>
       ) : (
         <div
           className="gradient gradient-background"
           style={{ "--degree": `${210}deg` }}
         >
-          <Home/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/events" element={<Events />} />
+          </Routes>
         </div>
       )}
-    </>
+    </BrowserRouter>
   );
-} 
+}
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 import { logo } from "../assets/images";
 import { navborder } from "../assets/svg";
 
 const Navbar = () => {
   return (
 
-    <div className='hidden sm:flex relative p-3  items-center'>
+    <div className='hidden sm:flex relative p-3 mb-10 items-center'>
 
-      <img src={logo} height={150} width={150} alt='logo' className='mr-4' />
+      <img src={logo} height={200} width={180} alt='logo' className='mr-4' />
       <div className='relative z-10 text-White flex gap-24 p-4 w-1/3'>
-        <h1 className='text-white hover:text-gray-300 cursor-pointer'>Events</h1>
-        <h1 className='text-white hover:text-gray-300 cursor-pointer'>About</h1>
-        <h1 className='text-white hover:text-gray-300 cursor-pointer'>Committee</h1>
+        <Link to="/events" className='text-white hover:text-gray-300 cursor-pointer'>Events</Link>
+        <Link to="/about" className='text-white hover:text-gray-300 cursor-pointer'>About</Link>
+        <Link to="/committee" className='text-white hover:text-gray-300 cursor-pointer'>Committee</Link>
 
       </div>
       <img src={navborder} alt='logo' className='absolute top-0 left-0 z-0 w-1/2' />
